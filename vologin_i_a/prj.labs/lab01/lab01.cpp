eb4186ed2f05cb2314dc1010263ddf026547da49
Ivan <2001joker2001@gmail.com>
2022-02-11 17:36:03 +0300

    lab01 done

--- vologin_i_a/prj.labs/lab01/lab01.cpp
+++ vologin_i_a/prj.labs/lab01/lab01.cpp
@@ -1,46 +1,51 @@
 #include <opencv4/opencv2/opencv.hpp>
+#include <chrono>
 
 #define WIGHT 768
 #define HIGHT 180
 #define GAMMA1 2.2
 #define GAMMA2 2.4
 
-int main(){
+int main()
+{
   cv::Mat m(HIGHT, WIGHT, CV_8UC1);
+  m = 0;
   cv::Rect2d rc = {0, 0, 768, 60};
-  float invGamma1 = 1.0f / GAMMA1;
-  float invGamma2 = 1.0f / GAMMA2;
 
-  int CIndex{0};
-  for (int y{0}; y < HIGHT; ++y)
-    for (int x{0}; x < WIGHT; ++x)
-      m.at<uchar>(y, x) = x % 3 == 0 ? CIndex++ : CIndex;
+  for (ptrdiff_t y{0}; y < HIGHT; ++y)
+    for (ptrdiff_t x{0}; x < WIGHT; ++x)
+      m.at<uchar>(y, x) = x / 3;
 
   cv::rectangle(m, rc, {150}, 1);
   rc.y += rc.height;
 
+  auto start = std::chrono::high_resolution_clock::now();
   cv::Mat tmp{m};
-  tmp.convertTo(tmp,CV_64FC1);
-  tmp = (tmp/255.0f);
-  cv::pow(tmp,invGamma1,tmp);
-  tmp = tmp * 255.0f;
-  tmp.convertTo(tmp,CV_8UC1);
+  tmp.convertTo(tmp, CV_32FC1, 1.0f / 255.0f);
+  cv::pow(tmp, GAMMA1, tmp);
+  tmp.convertTo(tmp, CV_8UC1, 255.0f);
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  std::cout << "Time for G1 = " << duration.count() << " ms"
+            << "\n";
 
-  for (int y = rc.y; y < HIGHT - rc.height; ++y)
-    for (int x{0}; x < WIGHT; ++x)
-      m.at<uchar>(y, x) =  tmp.at<uchar>(y,x);
- 
+  tmp(rc).copyTo(m(rc));
+
   cv::rectangle(m, rc, {150}, 1);
   rc.y += rc.height;
 
-  for (int y = rc.y; y < HIGHT; ++y)
-    for (int x{0}; x < WIGHT; ++x)
-      m.at<uchar>(y, x) = pow(m.at<uchar>(y, x) / 255.0f, invGamma2) * 255.0f;
-      
+  start = std::chrono::high_resolution_clock::now();
+  for (ptrdiff_t y = rc.y; y < HIGHT; ++y)
+    for (ptrdiff_t x{0}; x < WIGHT; ++x)
+      m.at<uchar>(y, x) = pow(m.at<uchar>(y, x) / 255.0f, GAMMA2) * 255.0f;
+
+  stop = std::chrono::high_resolution_clock::now();
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(stop-start);
+  std::cout << "Time for G2 = " << duration.count() << " ms"
+            << "\n";
 
   cv::rectangle(m, rc, {150}, 1);
 
-  cv::imshow("lab01",m);
+  cv::imshow("lab01", m);
   cv::waitKey(0);
 }
-
--- vologin_i_a/prj.labs/lab02/lab02.cpp
+++ vologin_i_a/prj.labs/lab02/lab02.cpp
@@ -1,4 +1,9 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
+    cv::Mat img = cv::imread("../data/cross_0256x0256.png");
+    cv::imwrite("../data/cross_0256x0256.jpg", img,{cv::IMWRITE_JPEG_CHROMA_QUALITY,10});
+    cv::Mat imgl = cv::imread("cross_0256x0256.jpg");
+
+    cv::waitKey(0);
 }
