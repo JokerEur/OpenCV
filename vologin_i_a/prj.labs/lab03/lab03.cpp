4e894a6d6a4f1c7d3c2b9b9c795140cb57824b44
Ivan <31983366+JokerEur@users.noreply.github.com>
2022-02-24 14:36:17 +0300

    lab03 done

--- vologin_i_a/prj.labs/lab03/lab03.cpp
+++ vologin_i_a/prj.labs/lab03/lab03.cpp
@@ -3,13 +3,23 @@
 
 uint32_t BrightnessConvertion(const uint32_t color)
 {
-    return static_cast<uint32_t>(255 * 10 * fabs(sinf32(static_cast<float>(color) / (255.0f * 3.0f))) + 0.5f) % 256;
+    return static_cast<uint32_t>(128.0f * (sinf(color / 8.0f) + 1.0f));
 }
 
 int main()
 {
 
     cv::Mat InitialImage = cv::imread("../data/cross_0256x0256.png");
+    if (InitialImage.empty())
+        std::cout << "could not find image"
+                  << "\n";
+
+    cv::Mat m(180, 768, CV_8UC1);
+    m = 0;
+
+    for (ptrdiff_t y{0}; y < 180; ++y)
+        for (ptrdiff_t x{0}; x < 768; ++x)
+            m.at<uchar>(y, x) = x / 3;
 
     cv::imwrite("lab03_rgb.png", InitialImage);
 
@@ -22,10 +32,18 @@ int main()
     for (ptrdiff_t i{0}; i < 256; ++i)
         LookUpTable.at<uchar>(0, i) = BrightnessConvertion(i);
 
-    cv::Mat result_for_initial, result_for_greyscale;
-    cv::LUT(InitialImage, LookUpTable, result_for_initial);
-    cv::LUT(GreyScale, LookUpTable, result_for_greyscale);
+    cv::Mat ResultForInitial, ResultForGreyscale, ResultForChecker;
+    cv::LUT(m, LookUpTable, ResultForChecker);
+    cv::LUT(InitialImage, LookUpTable, ResultForInitial);
+    cv::LUT(GreyScale, LookUpTable, ResultForGreyscale);
 
-    cv::imwrite("lab03_gre_res.png", result_for_greyscale);
-    cv::imwrite("lab03_rgb_res.png", result_for_initial);
+    cv::imwrite("lab03_gre_res.png", ResultForGreyscale);
+    cv::imwrite("lab03_rgb_res.png", ResultForInitial);
+    cv::imwrite("lab03_checker.png", ResultForChecker);
+
+    cv::Mat FunctionVisualization(512, 512, CV_8UC1, 255);
+    for (ptrdiff_t i{0}; i < 512; i += 2)
+        FunctionVisualization.at<uchar>(512 - 2 * LookUpTable.at<uchar>(0, i / 2) - 1, i) = 0;
+
+    cv::imwrite("lab03_viz_func.png", FunctionVisualization);
 }
